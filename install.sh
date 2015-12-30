500059a8068cdfc40cc2d4fa1e9d0357c9f051eb
Ryohei Ueda <garaemon@gmail.com>
2015-12-30 13:49:09 +0900

    Use nautilus-open-terminal
    Modified:
      - install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install nautilus-scripts-manager nautilus-script-manager
+sudo apt-get install nautilus-scripts-manager nautilus-script-manager nautilus-open-terminal 
 
 mkdir -p ~/.gnome2/nautilus-scripts
 ln -sf $(pwd)/scripts/*.sh ~/.gnome2/nautilus-scripts
--- scripts/open-terminal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --working-directory="$PWD"
-
